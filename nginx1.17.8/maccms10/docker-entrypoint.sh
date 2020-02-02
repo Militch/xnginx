291a18fa7d1d6b666d3c334180235a7b9cac840f
Militch <litch.me@gmail.com>
2020-02-02 17:02:01 +0800

    fix(maccms): usage template config

--- nginx1.17.8/maccms10/docker-entrypoint.sh
+++ nginx1.17.8/maccms10/docker-entrypoint.sh
@@ -8,4 +8,6 @@ set \$NGINX_PHP_FPM_PORT $NGINX_PHP_FPM_PORT;
 
 EOF
 
+cat default_template.conf | sed "s/%MACCMS_ADMIN_FILENAME%/${MACCMS_ADMIN_FILENAME}/g" > /etc/nginx/conf.d/default.conf
+
 exec "$@"
